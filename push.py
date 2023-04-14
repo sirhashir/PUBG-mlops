5ad5d6b535207bffdf499c5ff9bc7c7eb91025cd
Yuvraj Sharma <ys8484529@gmail.com>
2023-04-14 11:15:11 +0530

    Create folder

--- push.py
+++ push.py
@@ -7,23 +7,21 @@ g = Github(os.environ['FULL_ACCESS'])
 # Get the repository that you want to work with
 repo = g.get_repo("Yuvraj-Sharma-2000/ec2")
 
-# Define the folder and file names
-folder_name = "I did it"
-file_name = "Katze sensi.txt"
+# Create a folder inside the repository
+folder_name = "I did it again"
+file_name = "Katze.txt"
 file_path = f"{folder_name}/{file_name}"
 
-# Delete the folder and its contents if it already exists
 try:
-    folder = repo.get_contents(folder_name)
-    repo.delete_file(folder.path, "Deleting folder", folder.sha)
-    print(f"Deleted folder {folder_name}")
+    # Try to get the contents of the file
+    file_contents = repo.get_contents(file_path)
+    # Update the contents of the file
+    updated_contents = "This is the updated content of the file"
+    # Commit the changes to the file
+    repo.update_file(file_path, "Updated content", updated_contents, file_contents.sha)
+    print(f"Updated file {file_path}")
 except Exception as e:
-    pass
-
-# Create the new folder
-repo.create_file(folder_name, "Creating folder", "")
-
-# Create the file
-file_contents = "This is the new content of the file"
-repo.create_file(file_path, "Creating file", file_contents)
-print(f"Created file {file_path}")
+    # If the file does not exist, create it
+    print(f"File {file_path} not found, creating it")
+    repo.create_file(file_path, "Initial commit", "This is the content of the file")
+    print(f"Created file {file_path}")
