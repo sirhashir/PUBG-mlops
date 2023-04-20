d5a939531307f71b3326acb07e62135fd16c87d9
Yuvraj Sharma <ys8484529@gmail.com>
2023-04-20 16:36:07 +0530

    Test cases

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ def client():
 def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'<!DOCTYPE html>' in response.data
+    assert b'<!DOCTYPE html>' in response.data 
 
 def test_home(client):
     response = client.get('/predictdata')
