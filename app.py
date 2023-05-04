75d0db8e7474696a1fa11f519108b1bc9c041532
Yuvraj Sharma <ys8484529@gmail.com>
2023-05-04 23:16:15 +0530

    latest

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask,request,render_template, jsonify
 import numpy as np
 import pandas as pd
+from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
-
 from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application=Flask(__name__)
 
