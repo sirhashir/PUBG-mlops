3bdeecb126f16f5e5c343b0804660600c419aee7
Yuvraj Sharma <ys8484529@gmail.com>
2023-05-11 10:38:36 +0530

    Interaction on Public IPA

--- test.py
+++ test.py
@@ -18,25 +18,3 @@ def test_home(client):
     assert response.status_code == 200
     assert b'<!DOCTYPE html>' in response.data
     assert b'Player Stats' in response.data
-
-
-# def test_predict_datapoint_with_values(client):
-#     data = {
-#         'Headers': {
-#             'assists': 2,
-#             'boosts': 4,
-#             'headshotKills': 1,
-#             'kills': 5,
-#             'longestKill': 45,
-#             'matchDuration': 1500,
-#             'revives': 1,
-#             'teamKills': 0,
-#             'vehicleDestroys': 0,
-#             'walkDistance': 2000,
-#             'weaponsAcquired': 7,
-#             'matchType': 'duo-fpp'
-#         }
-#     }
-#     response = client.post('/predictdata', json=data)
-#     assert response.status_code == 200
-#     assert b'The prediction is' in response.data
