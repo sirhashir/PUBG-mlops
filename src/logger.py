041e9700c1987b4c78fe858095bb1c7d82b845b1
sirhashir <umairhashir89@gmail.com>
2023-05-12 15:22:37 +0530

    logger

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,28 @@
+# import logging
+# import os
+# from datetime import datetime
+
+# LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+# os.makedirs(logs_path,exist_ok=True)
+
+# LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+# # with open(LOG_FILE_PATH,'w'): pass
+
+# logging.basicConfig(
+#     filename=LOG_FILE_PATH,
+#     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+#     level=logging.INFO,
+# )
+
+# if __name__=="__main__":
+#     logging.info("Logging has started")
+
+
 import logging
-import os
 from datetime import datetime
+import os
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
@@ -8,13 +30,14 @@ os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-# with open(LOG_FILE_PATH,'w'): pass
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
+logging.basicConfig(format="%(asctime)s::%(levelname)s::%(message)s",
+                    level=logging.INFO,
+                    filename=LOG_FILE_PATH
+                    )
 
-if __name__=="__main__":
-    logging.info("Logging has started")
+# logging.info("info log")
+# logging.warning("this is warning log")
+# logging.error("this is error log")
+
+logging.info(1)
